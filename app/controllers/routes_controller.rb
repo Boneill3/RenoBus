b87b184d4ddd157a43be3902328ef8b58839c85b
Brian Tackitt <briantackitt@sbcglobal.net>
2014-10-25 20:51:07 -0700

    feat: show route

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,4 +2,11 @@ class RoutesController < ApplicationController
   def index
     @routes_json = Route.includes(directions: [:stops]).all.to_json(include: {directions: {include: :stops} }) 
   end
+
+  def show
+    route = Rails.cache.fetch("route-#{params[:id]}") do
+      Route.includes(directions: [:stops], paths: [:points]).find(params[:id]).to_json(include: {directions: {include: :stops}, paths: {include: :points}})
+    end
+    render json: route
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :routes, only: %i(index)
+  resources :routes, only: %i(index show)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
